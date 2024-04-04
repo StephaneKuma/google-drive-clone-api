b951efcc367c4307522d00e33ebd68cf42b03e77
Stéphane Kuma <kumastephane@gmail.com>
2024-04-04 17:11:51 +0000

    Add more tests for folder creation

--- tests/Feature/File/Folder/CreateFolderTest.php
+++ tests/Feature/File/Folder/CreateFolderTest.php
@@ -37,3 +37,62 @@ test('can create a folder', function () {
     expect($root->rgt)->toBe(4);
     expect($root->children->count())->toBe(1);
 });
+
+
+test('can not create a folder with invalid parent', function () {
+    postJson('/api/v1/register', [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
+
+    postJson('/api/v1/register', [
+        'name' => 'Test Second User',
+        'email' => 'test2@example.com',
+        'password' => 'password2',
+        'password_confirmation' => 'password2',
+    ]);
+
+    $user = User::where('email', 'test@example.com')->first();
+
+    actingAs($user);
+
+    $response = postJson('/api/v1/folders', [
+        'parent_id' => 2,
+        'name' => 'Test Folder',
+    ]);
+
+    $response->assertUnprocessable()
+        ->assertJsonValidationErrorFor('parent_id');
+});
+
+test('can not create a folder with already existing name', function () {
+    postJson('/api/v1/register', [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
+
+    $user = User::where('email', 'test@example.com')->first();
+
+    actingAs($user);
+
+    $root = File::query()->userRoot()->first();
+
+    postJson('/api/v1/folders', [
+        'parent_id' => $root->id,
+        'name' => 'Test Folder',
+    ]);
+
+    $response = postJson('/api/v1/folders', [
+        'parent_id' => $root->id,
+        'name' => 'Test Folder',
+    ]);
+
+    $response->assertForbidden()
+        ->assertJsonStructure([
+            'message'
+        ]);
+});
